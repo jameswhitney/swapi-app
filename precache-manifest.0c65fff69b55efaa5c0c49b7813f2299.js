af1d316363910f7c18332cf30a03b3e0fb916823
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2020-04-23 14:41:19 -0400

    Updates

---
+++ precache-manifest.0c65fff69b55efaa5c0c49b7813f2299.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d4cefd00c600df772b8ee209f23097",
+    "url": "/swapi-app/index.html"
+  },
+  {
+    "revision": "d77a14ebccddc80641c2",
+    "url": "/swapi-app/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "3d3b168f12cc97e6f035",
+    "url": "/swapi-app/static/css/main.1395cd6a.chunk.css"
+  },
+  {
+    "revision": "d77a14ebccddc80641c2",
+    "url": "/swapi-app/static/js/2.f9f633e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swapi-app/static/js/2.f9f633e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3b168f12cc97e6f035",
+    "url": "/swapi-app/static/js/main.ccb74a7d.chunk.js"
+  },
+  {
+    "revision": "368222e33031c6d86abc",
+    "url": "/swapi-app/static/js/runtime-main.9e0b0bcc.js"
+  },
+  {
+    "revision": "a34484e451f48ba907f138318c3a5073",
+    "url": "/swapi-app/static/media/jaapokkisubtract-regular.a34484e4.woff"
+  }
+]);
